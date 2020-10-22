937c2920b265b28d2e7560a629ff5aff955063b0
Arthur De Witte <arthur.dewitte@gmail.com>
2020-10-22 19:45:00 +0200

    Little filter bugg fix

--- legends.php
+++ legends.php
@@ -10,9 +10,7 @@ if (isset($_GET["school"])) {
     foreach ($userHandler->users as $user) {
         if ($user->school == $_GET['school']) $users[] = $user;
     }
-}
-
-if (!isset($users)) $users = $userHandler->users;
+} else $users = $userHandler->users;
 
 ?>
 
@@ -32,33 +30,35 @@ if (!isset($users)) $users = $userHandler->users;
 <body>
     <?php include getcwd() . "/includes/header.php"; ?>
     <main>
-        <section class="wrapper">
-            <?php foreach ($users as $user) : ?>
-                <section class="user" <?php echo isLoggedIn() ? (unserialize($_SESSION['user'])->id == $user->id ? "style='background-color: #FFD700'" : "") : "" ?>>
-                    <div class="vote-wrapper">
-                        <button onclick="upVote('<?php echo $user->id; ?>')">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                <path d="M24 12l-12-9v5h-12v8h12v5l12-9z" />
-                            </svg>
-                        </button>
-                        <button onclick="downVote('<?php echo $user->id; ?>')">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                <path d="M24 12l-12-9v5h-12v8h12v5l12-9z" />
-                            </svg>
-                        </button>
-                    </div>
-                    <a href="./legend.php?legend=<?php echo $user->id ?>" class="user-wrapper">
-                        <div class="img-container">
-                            <img src="<?php echo $user->avatar; ?>" alt="<?php echo $user->nickname; ?>">
+        <?php if (isset($users)) : ?>
+            <section class="wrapper">
+                <?php foreach ($users as $user) : ?>
+                    <section class="user" <?php echo isLoggedIn() ? (unserialize($_SESSION['user'])->id == $user->id ? "style='background-color: #FFD700'" : "") : "" ?>>
+                        <div class="vote-wrapper">
+                            <button onclick="upVote('<?php echo $user->id; ?>')">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path d="M24 12l-12-9v5h-12v8h12v5l12-9z" />
+                                </svg>
+                            </button>
+                            <button onclick="downVote('<?php echo $user->id; ?>')">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path d="M24 12l-12-9v5h-12v8h12v5l12-9z" />
+                                </svg>
+                            </button>
                         </div>
-                        <div class="text-container">
-                            <h1><?php echo $user->nickname; ?></h1>
-                            <h2><?php echo $schoolHandler->getSchool($user->school)->full_name ?></h2>
-                        </div>
-                    </a>
-                </section>
-            <?php endforeach; ?>
-        </section>
+                        <a href="./legend.php?legend=<?php echo $user->id ?>" class="user-wrapper">
+                            <div class="img-container">
+                                <img src="<?php echo $user->avatar; ?>" alt="<?php echo $user->nickname; ?>">
+                            </div>
+                            <div class="text-container">
+                                <h1><?php echo $user->nickname; ?></h1>
+                                <h2><?php echo $schoolHandler->getSchool($user->school)->full_name ?></h2>
+                            </div>
+                        </a>
+                    </section>
+                <?php endforeach; ?>
+            </section>
+        <?php endif; ?>
     </main>
     <script src="./js/vote.js"></script>
 </body>
