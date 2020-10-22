b2ca49b6d43d609e961fe40657f1b20c9c0abb37
Arthur De Witte <arthur.dewitte@gmail.com>
2020-10-22 17:39:16 +0200

    Updated the users page.

--- legends.php
+++ legends.php
@@ -14,6 +14,7 @@ $userHandler = new UsersHandler(getcwd() . users_file);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>School Legends | Legends</title>
     <link rel="stylesheet" href="./style/general.css">
+    <link rel="stylesheet" href="./style/users.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Piazzolla:ital,wght@1,800&display=swap" rel="stylesheet">
 </head>
@@ -24,17 +25,25 @@ $userHandler = new UsersHandler(getcwd() . users_file);
         <?php foreach ($userHandler->users as $user) : ?>
             <section>
                 <div class="vote-wrapper">
-                    <button onclick="upVote('<?php echo $user->id; ?>')"></button>
-                    <button onclick="downVote('<?php echo $user->id; ?>')"></button>
+                    <button onclick="upVote('<?php echo $user->id; ?>')">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M24 12l-12-9v5h-12v8h12v5l12-9z" />
+                        </svg>
+                    </button>
+                    <button onclick="downVote('<?php echo $user->id; ?>')">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M24 12l-12-9v5h-12v8h12v5l12-9z" />
+                        </svg>
+                    </button>
                 </div>
-                <div class="user-wrapper">
+                <a href="./legend.php?legend=<?php echo $user->id ?>" class="user-wrapper">
                     <div class="img-container">
                         <img src="<?php echo $user->avatar; ?>" alt="<?php echo $user->nickname; ?>">
                     </div>
                     <div class="text-container">
                         <h1><?php echo $user->nickname; ?></h1>
                     </div>
-                </div>
+                </a>
             </section>
         <?php endforeach; ?>
     </main>
