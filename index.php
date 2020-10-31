192729287289522da7501ca29ba1a830c989b2f9
Arthur De Witte <arthur.dewitte@gmail.com>
2020-10-31 12:55:22 +0100

    Added index

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ include getcwd() . "/includes/fileHandler.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>School Legends | Home</title>
     <link rel="stylesheet" href="./style/general.css">
+    <link rel="stylesheet" href="./style/index.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Piazzolla:ital,wght@1,800&display=swap" rel="stylesheet">
 </head>
@@ -19,7 +20,16 @@ include getcwd() . "/includes/fileHandler.php";
 <body>
     <?php include getcwd() . "/includes/header.php"; ?>
     <main>
-
+        <div class="wrapper">
+            <h1>Everyone has that one or more legends in their school, view and vote for them here!</h1>
+            <h4>Sitemap:</h4>
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="./legends.php">View all the legends!</a></li>
+                <li><a href="./scholen.php">View all the schools!</a></li>
+                <li><a href="./login.php">Sign In!</a></li>
+            </ul>
+        </div>
     </main>
 </body>
 
